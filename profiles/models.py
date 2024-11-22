335def8a96d0f97d8b5da961864fa6bde1332918
bennminn <benjapavez000@gmail.com>
2024-11-21 22:40:39 -0300

    Modelo actualizado

--- profiles/models.py
+++ profiles/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Job_category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name_job = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name_job
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     photo = models.ImageField(blank=True, upload_to='photos')
     bio = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
-    jobtitle = models.TextField()
+    jobtitle = models.ForeignKey(Job_category, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
-        return f"profile of {self.user.username}"
+        return f"Profile of {self.user.username}"
+
+
